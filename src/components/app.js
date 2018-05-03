8c2d6f3c2074a4015959e0e1f384b6d214b12193
cyaah <calvin.yah@gmail.com>
2018-05-03 16:56:27 -0400

    Created a connection toyoutube api and started to bring data over

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './search_bar';
+import VideoList from './video_list';
+import VideoListItem from './video_list_item';
+
+
+const ApiKey = 'AIzaSyA8rUCDXexRg9Tp7l3O35l-tFb26Oo9AC8	';
+
 
 export default class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+		YTSearch({key: ApiKey, term:'surfboards'}, (videos) => { 
+			this.setState({ videos });
+		}); 
+	}
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+      <SearchBar />
+      <VideoList videos={this.state.videos} />
+      </div>
     );
   }
 }
